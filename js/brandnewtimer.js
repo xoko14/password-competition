cb2b9736a937b4cb92310bf965e3526e1f4148cd
Xoko14 <xoquinperezb@gmail.com>
2018-11-13 17:50:26 +0100

    Provisional design done
    meh

--- js/brandnewtimer.js
+++ js/brandnewtimer.js
@@ -1,14 +1,16 @@
+var timer = false;
+
 function go() {
-    i = 60;
-    var myInterval = setInterval(function() {
-        $(".progress-bar").attr("aria-valuenow", i/60*100);
-        $(".progress-bar").css("width",i/60*100 + "%");
-        $("#timeleft").html(i);
-        if (i == 0) {
-            $("#timeleft").html("Complete!");
-            clearInterval(myInterval);
-        }
-        i--;
+  clearInterval(timer);
+  i = 60;
+  timer = setInterval(function() {
+    $(".progress-bar").attr("aria-valuenow", i / 60 * 100);
+    $(".progress-bar").css("width", i / 60 * 100 + "%");
+    $("#timeleft").html(i);
+    if (i == 0) {
+      $("#timeleft").html("0");
+      clearInterval(timer);
     }
-    ,1000);
+    i--;
+  }, 1000);
 }
--- js/key.js
+++ js/key.js
@@ -1,42 +1,45 @@
 function inicializar() {
-	document.onkeydown = function () {
-		switch (event.keyCode) {
-			case 32 :
-			    establecerCuentaAtras();
-				imprimirEnPantallaLosResutados();
-			break;
-			case 81 :
-				arteixoPlus();
-			break;
-			case 65 :
-				arteixoLess();
-			break;
-			case 87 :
-				ordesPlus();
-			break;
-			case 83 :
-				ordesLess();
-			break;
-			case 69 :
-				larachaPlus();
-			break;
-			case 68 :
-				larachaLess();
-			break;
-			case 77 :
-				//playRandomSong();
-			break;
-			case 78 :
-				//stopSong();
-			break;
-			default :
-				return true;
-			break;
-		}
-		event.keyCode = 0;
-		event.returnValue = false;
-		event.cancelBubble = true;
-		return false;
-	}
-	document.onhelp = function () { alert("F1"); return false; }
+  document.onkeydown = function() {
+    switch (event.keyCode) {
+      case 32:
+        go();
+        imprimirEnPantallaLosResutados();
+        break;
+      case 81:
+        arteixoPlus();
+        break;
+      case 65:
+        arteixoLess();
+        break;
+      case 87:
+        ordesPlus();
+        break;
+      case 83:
+        ordesLess();
+        break;
+      case 69:
+        larachaPlus();
+        break;
+      case 68:
+        larachaLess();
+        break;
+      case 77:
+        //playRandomSong();
+        break;
+      case 78:
+        //stopSong();
+        break;
+      default:
+        return true;
+        break;
+    }
+    event.keyCode = 0;
+    event.returnValue = false;
+    event.cancelBubble = true;
+    return false;
+  }
+  document.onhelp = function() {
+    alert("F1");
+    return false;
+  }
 }
