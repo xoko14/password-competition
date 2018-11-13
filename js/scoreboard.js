722f7b178fbc3894a80e6911edb4bc57da33960a
Xoko14 <xoquinperezb@gmail.com>
2018-11-13 15:42:25 +0100

    test123
    test123

---
+++ js/scoreboard.js
@@ -0,0 +1,39 @@
+var laracha = 0 ;
+var arteixo = 0 ;
+var ordes = 0 ;
+var larachap = document.getElementById("larachap");
+console.log(larachap);
+var ordesp = document.getElementById("ordesp");
+console.log(ordesp);
+var arteixop = document.getElementById("arteixop");
+console.log(arteixop);
+
+function larachaPlus(){
+	laracha = laracha + 1; 
+	larachap.innerHTML = laracha;
+}
+
+function larachaLess(){
+	laracha = laracha - 1; 
+	larachap.innerHTML = laracha;
+}
+
+function ordesPlus(){
+	ordes = ordes + 1; 
+	ordesp.innerHTML = ordes;
+}
+
+function ordesLess(){
+	ordes = ordes - 1; 
+	ordesp.innerHTML = ordes;
+}
+
+function arteixoPlus(){
+	arteixo = arteixo + 1; 
+	arteixop.innerHTML = arteixo;
+}
+
+function arteixoLess(){
+	arteixo = arteixo - 1; 
+	arteixop.innerHTML = arteixo;
+}
---
+++ js/words.js
@@ -0,0 +1,49 @@
+
+
+var numerosUsados = [];
+var palabrasSelecionadas = [];
+
+var words = document.getElementById("words");
+console.log(words);
+
+
+function obtenerNumeroAleatorioEntre(minimo, maximo){
+	return parseInt(Math.random() * (maximo - minimo) + minimo);
+}
+
+
+function obtenerPalabrasAleatorias(){
+	
+	numerosUsados = [];
+	palabrasSelecionadas = [];
+	
+	while(palabrasSelecionadas.length < 5){
+		
+		this.numeroActual = obtenerNumeroAleatorioEntre(0, 779); // Del cero al 9 hay 10 numeros
+		
+		for(var n in numerosUsados){ // Repasar todos los numeros que ya se hayan pillado
+			
+			if(this.numeroActual == numerosUsados[n]){
+				this.numeroActual = -1;
+			}
+			
+		}
+		
+		if(this.numeroActual >= 0){
+			
+			palabrasSelecionadas.push(palabras[this.numeroActual]);
+			numerosUsados.push(this.numeroActual);
+			
+		}
+		
+	}
+	
+	return palabrasSelecionadas;
+	
+}
+
+function imprimirEnPantallaLosResutados(){
+	
+	words.innerHTML = obtenerPalabrasAleatorias().join(", ");
+	
+}
