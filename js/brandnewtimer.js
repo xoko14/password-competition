7e10015d62e9a602b656c4358285d433c4b9a651
Xoko14 <xoquinperezb@gmail.com>
2018-11-14 19:50:03 +0100

    timer update
    Better timer behaviour

--- js/brandnewtimer.js
+++ js/brandnewtimer.js
@@ -9,13 +9,13 @@ function resetTimer(){
 function go() {
   document.getElementById("stress").classList.add('progress-bar-animated');
   clearInterval(timer);
-  i = 60;
+  i = 59;
   timer = setInterval(function() {
     $(".progress-bar").attr("aria-valuenow", i / 60 * 100);
     $(".progress-bar").css("width", i / 60 * 100 + "%");
     $("#timeleft").html(i);
     if (i == 0) {
-      $("#timeleft").html("0");
+      $("#timeleft").html("60");
       clearInterval(timer);
       resetTimer();
     }
