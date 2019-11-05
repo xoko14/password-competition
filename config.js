d405ffa0e0b5808824c69e743af9e1bfe43936b9
Xoko14 <xoquinperezb@gmail.com>
2019-11-05 18:07:29 +0100

    Config file
    added config file in the root directory

---
+++ config.js
@@ -0,0 +1,6 @@
+var titulo = "Password"
+var introText = "Welcome!"
+
+var team1 = "Team 1"
+var team2 = "Team 2"
+var team3 = "Team 3"
---
+++ js/webinit.js
@@ -0,0 +1,4 @@
+document.getElementById("words").innerHTML = introText
+document.getElementById("team1").innerHTML = team1
+document.getElementById("team2").innerHTML = team2
+document.getElementById("team3").innerHTML = team3
