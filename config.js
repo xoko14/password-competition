f576a073ef7ccdc5ffcb10d0a32c319293e2363b
Xoko14 <xoquinperezb@gmail.com>
2019-11-05 18:32:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-//cambiar sólo texto entre ""
-var introText = "Welcome!" //Texto mostrado en vez de palabras al abrir
+//cambiar sólo texto entre "" (<br> significa salto de línea)
+var introText = "ÁRTABRA PASSWORD COMPETITION" //Texto mostrado en vez de palabras al abrir
 
-var team1 = "Team 1" //nombre equipo 1
-var team2 = "Team 2" //nombre equipo 2
-var team3 = "Team 3" //nombre equipo 3
+var team1 = "María <br> Casares" //nombre equipo 1
+var team2 = "Neira <br> Vilas" //nombre equipo 2
+var team3 = "Díaz <br> Pardo" //nombre equipo 3
