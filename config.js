52da2ddfbe63737fb3b52d73ca5eabd9cb9bb259
Xoko14 <xoquinperezb@gmail.com>
2019-11-05 18:17:18 +0100

    ok

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-var titulo = "Password"
-var introText = "Welcome!"
+//cambiar sólo texto entre ""
+var introText = "Welcome!" //Texto mostrado en vez de palabras al abrir
 
-var team1 = "Team 1"
-var team2 = "Team 2"
-var team3 = "Team 3"
+var team1 = "Team 1" //nombre equipo 1
+var team2 = "Team 2" //nombre equipo 2
+var team3 = "Team 3" //nombre equipo 3
